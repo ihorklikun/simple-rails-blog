fdc8d054ab4bc12cd3db4ce895cc7c98b4980457
Ihor Klikun <klikun2001@gmail.com>
2020-05-04 15:53:39 +0300

    Fix db

--- config/application.rb
+++ config/application.rb
@@ -9,13 +9,8 @@ Bundler.require(*Rails.groups)
 module SimpleBlog
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.assets.initialize_on_precompile = false
+    #config.assets.initialize_on_precompile = false
     config.load_defaults 6.0
-<<<<<<< HEAD
-    
-=======
-    config.assets.initialize_on_precompile = false
->>>>>>> 49b7584c1bbdf1c4c0cae235f9636d46f04b6ca4
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
